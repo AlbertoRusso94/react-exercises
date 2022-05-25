8aea6416213108031235b3f30fdd7295c1d61dbc
Alberto Russo <alberto9449@gmail.com>
2022-05-25 11:30:01 +0200

    feature/JSX-02

--- src/React-Project.js
+++ src/React-Project.js
@@ -1,3 +1,12 @@
 import React from "react";
+/*esercizio 01 */
 
 const hello = <h1>Hello, world!</h1>;
+
+/*esercizio 02 */
+
+let nameFunction = (name) => <h1>Hello, {name} </h1>;
+
+/*esercizio 03*/
+
+let sumFunction = (a, b) => <h2> {a + b} </h2>;
