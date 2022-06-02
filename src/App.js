3b3eb848541d6e9d7421e3d38134af9f47a88d79
Alberto Russo <alberto9449@gmail.com>
2022-06-02 14:27:33 +0200

    Forms-01-r

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export class App extends React.Component {
   render() {
     return (
       <div>
-        <InteractiveWelcome name="Alberto" />
+        <InteractiveWelcome />
       </div>
     );
   }
