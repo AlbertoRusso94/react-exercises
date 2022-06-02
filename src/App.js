0c66d1a6a6a4fc72c55800dbb6143f276011cf44
Alberto Russo <alberto9449@gmail.com>
2022-06-02 15:50:45 +0200

    Forms-02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { InteractiveWelcome } from "./InteractiveWelcome";
+// import { InteractiveWelcome } from "./InteractiveWelcome";
+import { Login } from "./Login";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <InteractiveWelcome />
+        {/* <InteractiveWelcome /> */}
+        <Login />
       </div>
     );
   }
---
+++ src/Login.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: true,
+  };
+
+  handleInput = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    const type = event.target.type;
+    const checked = event.target.checked;
+    this.setState({
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInput}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handleInput}
+        ></input>
+        <input
+          name="remember"
+          type="checkbox"
+          checked={this.state.remember}
+          onChange={this.handleInput}
+        ></input>
+      </div>
+    );
+  }
+}
