97721ddf1893578842abfe3cfdc418fb533e2579
Alberto Russo <alberto9449@gmail.com>
2022-06-13 12:30:28 +0200

    useState-01

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,22 +1,39 @@
-import React from "react";
+import { useState } from "react";
 
-export class ClickCounter extends React.Component {
-  state = {
-    count: this.props.initialValue,
-  };
-  contatoreConBottone = () => {
-    this.setState((state) => {
-      return {
-        count: state.count + this.props.incrementValue,
-      };
-    });
-  };
-  render() {
-    return (
-      <div>
-        <h1>Counter: {this.state.count} </h1>
-        <button onClick={this.contatoreConBottone}>Click here</button>
-      </div>
-    );
+export function ClickCounter({ initialValue = 0 }) {
+  const [count, setCount] = useState(initialValue);
+
+  function contatoreConBottone() {
+    setCount(count + 1);
   }
+
+  return (
+    <div>
+      <h1>Counter: {count} </h1>
+      <button onClick={contatoreConBottone}>Click here</button>
+    </div>
+  );
 }
+
+// import React from "react";
+
+// export class ClickCounter extends React.Component {
+//   state = {
+//     count: this.props.initialValue,
+//   };
+//   contatoreConBottone = () => {
+//     this.setState((state) => {
+//       return {
+//         count: state.count + this.props.incrementValue,
+//       };
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h1>Counter: {this.state.count} </h1>
+//         <button onClick={this.contatoreConBottone}>Click here</button>
+//       </div>
+//     );
+//   }
+// }
