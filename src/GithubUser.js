daae3c00490493f6cd830d29456ddbd24a70d4a6
Alberto Russo <alberto9449@gmail.com>
2022-06-16 11:32:43 +0200

    fix/CustomHooks-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-  const { data } = useGithubUser(username);
+  const { data, loading, error } = useGithubUser(username);
   return (
     <div>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error</h1>}
       {data && data.name}
       <hr></hr>
       {data && data.type}
