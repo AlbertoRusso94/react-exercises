e08610328985cefbafdb5997e2975c116605e85e
Alberto Russo <alberto9449@gmail.com>
2022-06-21 18:03:01 +0200

    feature/ReactRouter-03

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { ClickCounter } from "./ClickCounter.js";
 import { GithubUser } from "./GithubUser.js";
+import { ShowGithubUser } from "./ShowGithubUser.js";
 
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        {/* <ClickCounter initialValue={0} incrementValue={1} /> */}
-        <GithubUser username="AlbertoRusso94" />
-      </div>
+      <Routes>
+        <Route path="/:username" element={<ShowGithubUser />} />
+      </Routes>
     );
   }
 }
