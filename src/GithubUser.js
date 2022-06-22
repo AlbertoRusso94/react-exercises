cdc5cfcd2b32ee216f5d7e1cacaf6657a5e1aa4a
Alberto Russo <alberto9449@gmail.com>
2022-06-22 16:15:41 +0200

    feature/SWR-01

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -3,11 +3,12 @@ import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
   const { data, loading, error } = useGithubUser(username);
+  console.log(data);
   return (
     <div>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error</h1>}
-      {data && data.name}
+      {data && <p> {data.name} </p>}
       <hr></hr>
       {data && data.type}
       <hr></hr>
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,34 +1,44 @@
-import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 export function useGithubUser(username) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          setError(new Error("User not found"));
-        }
-        return response.json();
-      })
-      .then((json) => {
-        setLoading(false);
-        setData(json);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [username]);
-  return {
-    data,
-    loading,
-    error,
-  };
+  const fetcher = (url) => fetch(url).then((response) => response.json());
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
+  return { data, error, loading: !data && !error };
 }
+
+// import { useEffect, useState } from "react";
+
+// export function useGithubUser(username) {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     setLoading(true);
+//     setError(null);
+//     fetch(`https://api.github.com/users/${username}`)
+//       .then((response) => {
+//         if (response.status !== 200) {
+//           setError(new Error("User not found"));
+//         }
+//         return response.json();
+//       })
+//       .then((json) => {
+//         setLoading(false);
+//         setData(json);
+//       })
+//       .catch((error) => {
+//         setError(error);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//       });
+//   }, [username]);
+//   return {
+//     data,
+//     loading,
+//     error,
+//   };
+// }
