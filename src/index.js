14ac3b7b3c52665cfa1e66b92397321bd497e7c4
Alberto Russo <alberto9449@gmail.com>
2022-06-23 12:40:49 +0200

    feature/Redux-03

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ import {
   incrementCounter,
   resetCounter,
 } from "./state/CounterState";
+import { addTodo, removeTodo, resetTodos } from "./state/TodoState";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 store.subscribe(() => {
   console.log(store.getState());
 });
-store.dispatch(incrementCounter(7));
-store.dispatch(decrementCounter(5));
-store.dispatch(resetCounter());
+
+store.dispatch(addTodo({ id: 1, title: "Buy phone", completed: true }));
+store.dispatch(addTodo({ id: 2, title: "Buy cover", completed: true }));
+store.dispatch(addTodo({ id: 3, title: "Buy tabacco", completed: false }));
+store.dispatch(removeTodo(1));
+store.dispatch(removeTodo(2));
+store.dispatch(resetTodos());
+// store.dispatch(incrementCounter(7));
+// store.dispatch(decrementCounter(5));
+// store.dispatch(resetCounter());
--- src/state/Store.js
+++ src/state/Store.js
@@ -1,4 +1,6 @@
 import { createStore } from "redux";
+import { TodoReducer } from "./TodoState";
 import { counterReducer } from "./CounterState";
 
-export const store = createStore(counterReducer);
+// export const store = createStore(counterReducer);
+export const store = createStore(TodoReducer);
