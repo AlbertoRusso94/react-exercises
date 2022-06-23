bbbd90315df503e44ba92435cfbda3eacf021692
Alberto Russo <alberto9449@gmail.com>
2022-06-23 12:10:53 +0200

    feature/Redux-02

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // import ReactDOM from "react-dom";
 // import { App } from "./App";
 // import "./index.css";
+import { store } from "./state/Store";
+import {
+  decrementCounter,
+  incrementCounter,
+  resetCounter,
+} from "./state/CounterState";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(incrementCounter(7));
+store.dispatch(decrementCounter(5));
+store.dispatch(resetCounter());
